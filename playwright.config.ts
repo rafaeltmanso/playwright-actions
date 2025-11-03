c89a988894f51680f9c50c55b454f08a49387eae
Rafael Manso <129555130+rafaeltmanso@users.noreply.github.com>
2025-11-03 14:19:23 -0300

    feat: add workflow_dispatch trigger, multibrowser tests and enable video recording in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test'
+import { on } from 'events'
 
 /**
  * Read environment variables from file.
@@ -31,6 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: 'https://loginxp.vercel.app',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -40,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
